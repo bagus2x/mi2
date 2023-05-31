1ca1fb5dad65b8464950cdf15ef21e16aa24bdc1
bagus2x <dutapanggulu@gmail.com>
2023-05-31 16:10:17 +0700

    add domain

--- client/src/app/about/teachers/page.tsx
+++ client/src/app/about/teachers/page.tsx
@@ -4,6 +4,7 @@ export default function Teachers() {
   return (
     <main className="max-w-screen-xl mx-auto w-full h-full px-4 py-4 xl:py-8">
       <SectionLabel label="Guru dan Karyawan" />
+      mashih kosong ya, mana datanya hayo. Lah wong gak dikasih
     </main>
   )
 }
